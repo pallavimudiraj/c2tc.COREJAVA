0353a4f250eb18458d4f5da0241d1123cae2f752
Pallavimudiraj <Pallavimudiraj@DESKTOP-AJVK21D>
2022-10-19 20:40:51 +0530

    first commit

---
+++ com.JavaDemo/src/com/JavaDemo/JavaCodeSixth.java
@@ -0,0 +1,18 @@
+package com.JavaDemo;
+
+public class JavaCodeSixth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				
+		       float f = 10.532f;
+		       long l = (long) f;
+		       System.out.println(l);
+			}
+		}
+
+
+	
+
+
