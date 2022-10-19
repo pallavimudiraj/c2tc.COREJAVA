46de7f73b6ceaadf89e9bddc03ee3c2d91937033
Pallavimudiraj <Pallavimudiraj@DESKTOP-AJVK21D>
2022-10-19 22:55:58 +0530

    first commit

---
+++ com.JavaDemo/src/com/JavaDemo/JavaCodeForLoop.java
@@ -0,0 +1,16 @@
+package com.JavaDemo;
+
+public class JavaCodeForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//This is the basic java program for loop 
+		for(int i = 1; i<=1000 ; i++)   //(int i = 1000; i<=1;i--)
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+
+	}
+
+}
+}
---
+++ com.JavaDemo/src/com/JavaDemo/JavaCodeSwitchCaseString.java
@@ -0,0 +1,26 @@
+package com.JavaDemo;
+
+public class JavaCodeSwitchCaseString {
+	public static void main(String[] args) {
+		//switch case (string)
+		String name = "te";
+
+		switch (name.toLowerCase()) 
+		{
+		case "author":
+			System.out.println("Vikas");
+			break;
+		case "team":
+			System.out.println("Team Java Full Stack");
+			break;
+		case "editor":
+			System.out.println("Vishnu & Krishna");
+			break;
+		default:
+			System.out.println("Invalid entry");
+			break;
+
+	}
+
+}
+}
