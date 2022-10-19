0d45aa4491c061bc1f1985c986082430e2622400
Pallavimudiraj <Pallavimudiraj@DESKTOP-AJVK21D>
2022-10-19 21:38:09 +0530

    first commit

---
+++ com.JavaDemo/src/com/JavaDemo/JavaTernaryOperator.java
@@ -0,0 +1,14 @@
+package com.JavaDemo;
+
+public class JavaTernaryOperator {
+	public static void main(String[] args)
+	{
+	
+	int x = 10;
+	
+	x = (x==10) ? 1 : 0;
+	
+	System.out.println(x);
+	}
+}
+
