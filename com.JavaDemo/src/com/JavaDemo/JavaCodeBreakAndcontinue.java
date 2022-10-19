732740ac38e319a0eb08e5b9276445b83a14f35e
Pallavimudiraj <Pallavimudiraj@DESKTOP-AJVK21D>
2022-10-19 23:34:39 +0530

    first commit

---
+++ com.JavaDemo/src/com/JavaDemo/JavaCodeBreakAndcontinue.java
@@ -0,0 +1,18 @@
+package com.JavaDemo;
+
+public class JavaCodeBreakAndcontinue {
+// Here in this we have make use of break and continue 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=10; i++)
+        {
+       	 if(i == 1)
+       	 {
+       		 continue;   // break;
+       	 }
+       	 System.out.println(i);
+        }
+
+	}
+
+}
