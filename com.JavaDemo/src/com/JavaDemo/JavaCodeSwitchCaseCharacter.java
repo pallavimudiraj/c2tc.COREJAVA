b4562e103c391dcb83dd9a571b25ea42fe589af1
Pallavimudiraj <Pallavimudiraj@DESKTOP-AJVK21D>
2022-10-19 22:34:17 +0530

    first commit

---
+++ com.JavaDemo/src/com/JavaDemo/JavaCodeSwitchCaseCharacter.java
@@ -0,0 +1,31 @@
+package com.JavaDemo;
+
+public class JavaCodeSwitchCaseCharacter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char x = 'F';
+		//switch case program (Char)
+		switch (x) 
+		{
+		case 'a' : 
+		case 'A' : 
+			System.out.println("The value of x is a");
+			break;
+		case 'b':
+		case 'B' :
+			System.out.println("The value of x is b");
+			break;
+		case 'c':
+		case 'C' :	
+			System.out.println("The value of x is c");
+			break;
+		default:
+	System.out.println("The value of x is other than a,b,c");
+			break;
+
+	}
+	}
+}
+
+
