69f1970947ac298998e248cc85d72c40bf0efadd
Pallavimudiraj <Pallavimudiraj@DESKTOP-AJVK21D>
2022-10-19 20:33:57 +0530

    first commit

---
+++ com.JavaDemo/src/com/JavaDemo/JavaCodeFive.java
@@ -0,0 +1,13 @@
+package com.JavaDemo;
+
+public class JavaCodeFive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String var = new String("Hello world");
+		System.out.println(var);
+
+
+	}
+
+}
