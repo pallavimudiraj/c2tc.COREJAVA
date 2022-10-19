7fe0608479f8e1197f9c2841944da05a297882f0
Pallavimudiraj <Pallavimudiraj@DESKTOP-AJVK21D>
2022-10-19 21:22:39 +0530

    first commit

---
+++ com.JavaDemo/src/com/JavaDemo/JavaCodeNine.java
@@ -0,0 +1,22 @@
+package com.JavaDemo;
+
+public class JavaCodeNine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 8, y = 7;
+		
+           if(x >= y)
+           {
+        	   System.out.println("true");
+           }
+           else
+           {
+        	   System.out.println("false");
+           }
+
+
+	}
+
+}
