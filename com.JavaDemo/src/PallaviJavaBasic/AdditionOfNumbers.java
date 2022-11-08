6a8c3e009dcb98963537fa8a25b8424c5f13b16b
Pallavimudiraj <Pallavimudiraj@DESKTOP-AJVK21D>
2022-11-08 19:41:47 +0530

    first commit

---
+++ com.JavaDemo/src/PallaviJavaBasic/AdditionOfNumbers.java
@@ -0,0 +1,17 @@
+package PallaviJavaBasic;
+
+public class AdditionOfNumbers {
+	public static void main (String args[]) {
+		
+	
+	
+	int num1 = 4;
+	int num2 = 2;
+	int num3 = 6;
+	int add;
+	
+	
+	add= num1+num2+num3;
+	System.out.println("sum = "+add);
+	}
+}
