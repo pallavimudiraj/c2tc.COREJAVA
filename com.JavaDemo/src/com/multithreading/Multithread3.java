fb07e8f55aca407d87fdf0b6574f3795218e5387
Pallavimudiraj <Pallavimudiraj@DESKTOP-AJVK21D>
2023-01-05 11:44:14 +0530

    4th commit

---
+++ com.JavaDemo/src/com/multithreading/Multithread3.java
@@ -0,0 +1,24 @@
+package com.multithreading;
+
+class Eclipse implements Runnable{
+	
+	public void run()
+	{
+	    
+		System.out.println("Eclipse Thread name is"+ Thread.currentThread().getName());
+		System.out.println("Eclipse Thread ID is" + Thread.currentThread().getId());
+	}
+}
+
+public class Multithread3 {
+	
+	public static void main(String[] args) {
+		Eclipse e=new Eclipse();
+		Thread t=new Thread(e);
+		t.start();
+	        
+			System.out.println("Main Thread name is" + Thread.currentThread().getName());
+			System.out.println("Main Thread ID is" + Thread.currentThread().getId());
+
+		}
+	}
